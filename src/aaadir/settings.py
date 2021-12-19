263946bc9746b9e766b3950fc1b168aa83e7a208
coolnamz <coolnamz@naver.com>
2021-12-19 12:46:41 +0000

    remove node install & npm install

--- src/aaadir/settings.py
+++ src/aaadir/settings.py
@@ -17,6 +17,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Load environ file
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False),
@@ -27,6 +29,7 @@ environ.Env.read_env(
     env_file=os.path.join(BASE_DIR, 'django.env')
 )
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -36,6 +39,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = int(env("DEBUG", default=0))
 ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(" ")
 SITE_ID = int(env("SITE_ID", default=1))
+
 
 # Application definition
 
@@ -85,28 +89,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'aaadir.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-if int(env("DEV")): 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': env("MYSQL_DATABASE"),
-            'USER': env("MYSQL_USER"), 
-            'PASSWORD': env("MYSQL_PASSWORD"),
-            'HOST': env("MYSQL_HOST"), 
-            'PORT': env("MYSQL_PORT"), 
-        }
-    }   
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -124,6 +106,20 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'), 
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+    
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# FILE_UPLOAD_PERMISSIONS=0o640
 
 
 # Internationalization
@@ -162,29 +158,35 @@ REST_FRAMEWORK = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# SSL
-if int(env("HTTPS")):
-    SECURE_SSL_REDIRECT = False
-    SESSION_COOKIE_SECURE = False
-    CSRF_COOKIE_SECURE = False
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-if int(env("DEV")):
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'staticfiles'), 
-    ]
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
-
+if int(env("DEV")): 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 else:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
-    
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# FILE_UPLOAD_PERMISSIONS=0o640
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': env("MYSQL_DATABASE"),
+            'USER': env("MYSQL_USER"), 
+            'PASSWORD': env("MYSQL_PASSWORD"),
+            'HOST': env("MYSQL_HOST"), 
+            'PORT': env("MYSQL_PORT"), 
+        }
+    }  
+
+
+# SSL
+# if int(env("HTTPS")):
+#     SECURE_SSL_REDIRECT = False
+#     SESSION_COOKIE_SECURE = False
+#     CSRF_COOKIE_SECURE = False
+
 
 # Email
 EMAIL_USE_TLS = int(env("EMAIL_USE_TLS"))
